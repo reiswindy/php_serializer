60777b229e7b5c9230ae1220a847fa4bdf7f759a
reiswindy <megamanbn@gmail.com>
2018-06-28 18:21:22 -0500

    Fix: Do not use verbatime, use macors in simple method

--- src/php_serialize/serialization.cr
+++ src/php_serialize/serialization.cr
@@ -1,18 +1,14 @@
 module PHP::Serializable
-  macro included
 
-    {% verbatim do %}
-
-      def to_php_serialized(php : PHP::Builder)
-        {% s = @type.instance_vars.size %}
-        php.object(self, {{s}}) do
-          {% for ivar in @type.instance_vars %}
-            php.named_property({{ivar.id.stringify}}, @{{ivar}})
-          {% end %}
-        end
+  def to_php_serialized(php : PHP::Builder)
+    {% begin %}
+      {% s = @type.instance_vars.size %}
+      php.object(self, {{s}}) do
+        {% for ivar in @type.instance_vars %}
+          php.named_property({{ivar.id.stringify}}, @{{ivar}})
+        {% end %}
       end
-
     {% end %}
-
   end
+
 end
